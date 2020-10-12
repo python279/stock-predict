7dc4031d81d942c4354b028828865a9eba067562
lhq <lhq@python279.org>
2020-10-13 06:48:41 +0800

    remove matplotlib and predict in future 2 days

--- download_train_predict.py
+++ download_train_predict.py
@@ -6,15 +6,12 @@ import sys
 from concurrent.futures import ThreadPoolExecutor
 from datetime import date, timedelta, datetime
 
-import matplotlib.pyplot as plt
 import mxnet
 import pandas as pd
 import requests
 from gluonts.dataset import common
 from gluonts.model import deepar
 from gluonts.trainer import Trainer
-import gc
-
 
 def download_train_predict(*args):
     def download_code_data(code, start_date, end_date, data_path, code_data_length_limit=20000):
@@ -38,7 +35,8 @@ def download_train_predict(*args):
                 return True
         return False
 
-    def train_and_predict(code, data_path, predict_path):
+    def train_and_predict(code, start_date, end_date, data_path, predict_path):
+        predict_days = 2
         csv = os.path.join(data_path, '{code}.csv'.format(code=code))
         df = pd.read_csv(csv)
 
@@ -62,8 +60,7 @@ def download_train_predict(*args):
 
         # fill the lost DT and label (TCLOSE) with last available exchange day's value
         all_dt = [(datetime.strptime(df.index[0], "%Y-%m-%d") + timedelta(days=i)).__format__('%Y-%m-%d') for i in
-                  range(1, (datetime.strptime(df.index[-1], "%Y-%m-%d") - datetime.strptime(df.index[0],
-                                                                                            "%Y-%m-%d")).days)]
+                  range(1, (datetime.strptime(end_date, "%Y%m%d") - datetime.strptime(df.index[0], "%Y-%m-%d")).days)]
         miss_data = []
         value = df.TCLOSE[df.index[0]]
         for dt in all_dt:
@@ -90,38 +87,32 @@ def download_train_predict(*args):
 
         # now training the model
         if len(mxnet.test_utils.list_gpus()):
-            estimator = deepar.DeepAREstimator(freq='1d', prediction_length=20, trainer=Trainer(ctx='gpu', epochs=100))
+            estimator = deepar.DeepAREstimator(freq='1d', prediction_length=predict_days, trainer=Trainer(ctx='gpu', epochs=100))
         else:
-            estimator = deepar.DeepAREstimator(freq='1d', prediction_length=20, trainer=Trainer(epochs=100))
+            estimator = deepar.DeepAREstimator(freq='1d', prediction_length=predict_days, trainer=Trainer(epochs=100))
         predictor = estimator.train(training_data=data)
 
         # predict the future data
         predict = predictor.predict(data, 1)
         predict_list = list(predict)
-        max, min, max_id, min_id = predict_list[0].samples.max(), predict_list[0].samples.min(), predict_list[
-            0].samples.argmax(), predict_list[0].samples.argmin()
-        plt.rcParams.update({'font.size': 40})
-        fig = plt.figure(figsize=(60, 20))
-        ax, bx = fig.subplots(1, 2, sharey=True)
-        ax.set_title(code, fontsize=40)
-        ax.plot(train_data.index[-20:], train_data.TCLOSE[-20:], 'g')
-        bx.set_title(code + "-predict", fontsize=40)
-        predict_x = [(predict_list[0].start_date + timedelta(days=i)).__format__('%Y-%m-%d') for i in
-                     range(1, 20 + 1)]
+        max, min, max_id, min_id = predict_list[0].samples.max(), predict_list[0].samples.min(), predict_list[0].samples.argmax(), predict_list[0].samples.argmin()
+        predict_x = [(predict_list[0].start_date + timedelta(days=i)).__format__('%Y-%m-%d') for i in range(0, predict_days + 1)]
         predict_y = predict_list[0].samples[0]
-        bx.plot(predict_x, predict_y, 'r')
-        bx.set_xticks(predict_x)
-        if min_id < max_id and (max - min) / min >= 0.2:
-            fig.savefig(os.path.join(predict_path, 'red_{code}.jpeg'.format(code=code)))
+        predict_df = pd.DataFrame(zip(pd.to_datetime(predict_x), predict_y), columns=['DT', 'TCLOSE'])
+        predict_df['timestamp'] = predict_df['DT']
+        predict_df.set_index('timestamp', inplace=True)
+        train_df = train_data.loc[train_data.index[-5:]]
+        train_df['DT'] = pd.to_datetime(train_df.index)
+        output_df = pd.concat([train_df, predict_df], axis=0)
+        if min_id < max_id and (max - min) / min >= 0.099:
+            output_df.to_csv(os.path.join(predict_path, 'red_{code}.csv'.format(code=code)))
         else:
-            fig.savefig(os.path.join(predict_path, 'green_{code}.jpeg'.format(code=code)))
+            output_df.to_csv(os.path.join(predict_path, 'green_{code}.csv'.format(code=code)))
         return True
 
     # function body
     code, start_date, end_date, data_path, predict_path = args[0]
-    ret = download_code_data(code, start_date, end_date, data_path) and train_and_predict(code, data_path, predict_path)
-    gc.collect()
-    return ret
+    return download_code_data(code, start_date, end_date, data_path) and train_and_predict(code, start_date, end_date, data_path, predict_path)
 
 
 if __name__ == "__main__":
@@ -140,6 +131,6 @@ if __name__ == "__main__":
     os.makedirs(data_path, exist_ok=True)
     predict_path = os.path.join(end_date, "predict")
     os.makedirs(predict_path, exist_ok=True)
-
-    with ThreadPoolExecutor(max_workers=10) as tpe:
+    
+    with ThreadPoolExecutor(max_workers=20) as tpe:
         tpe.map(download_train_predict, [(code, start_date, end_date, data_path, predict_path) for code in all_code])
